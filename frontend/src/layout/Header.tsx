577c1bfa6f94103f9e76b6d8b8b2990dee6d19cf
zubairkofficial <thezubairkhan.developer@gmail.com>
2025-06-17 18:41:29 +0500

    fix admin header

--- frontend/src/layout/Header.tsx
+++ frontend/src/layout/Header.tsx
@@ -2,22 +2,25 @@ import { Logo } from "@/components/Logo";
 import ProfileDropdown from "@/components/ProfileDropdown";
 import { Button } from "@/components/ui/button";
 import { useSidebar } from "@/components/ui/sidebar";
+import { useAuth } from "@/context/Auth.context";
 import { AlignLeft, PlusIcon, ZapIcon } from "lucide-react";
 
 export default function AppHeader() {
   const { toggleSidebar } = useSidebar()
+  const {user} = useAuth()
   return (
     <div className="md:flex bg-background md:sticky top-0 items-center md:justify-end px-4 h-22 z-20 gap-4 md:border-b">
-      <div className="md:flex hidden  items-center gap-4">
-        <p className="text-sm">Credit plan: 100/90</p>
+      {user.role !== "admin" && (
+        <div className="md:flex hidden  items-center gap-4">
+          <p className="text-sm">Credit plan: 100/90</p>
 
-        <Button className="hover:bg-transparent bg-orange py-2 h-[unset] hover:text-orange hover:shadow-none">
-          <ZapIcon />
-          Upgrade Plan
-        </Button>
-
-        <ProfileDropdown />
-      </div>
+          <Button className="hover:bg-transparent bg-orange py-2 h-[unset] hover:text-orange hover:shadow-none">
+            <ZapIcon />
+            Upgrade Plan
+          </Button>
+        </div>
+      )}
+      <ProfileDropdown />
       <div className="py-4 md:hidden flex items-center justify-between gap-4">
         <div className="w-full flex items-center gap-2">
           <Button className="justify-start" variant="ghost" size="icon" onClick={toggleSidebar}>
