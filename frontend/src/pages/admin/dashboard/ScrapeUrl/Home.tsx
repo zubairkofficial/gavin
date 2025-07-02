dabb23f6e3a7991460f3c6cf73d77e5eed98305c
Cyberify Developer <63253113+zubairkofficial@users.noreply.github.com>
2025-07-02 11:04:24 +0500

    items-center removed

--- frontend/src/pages/admin/dashboard/ScrapeUrl/Home.tsx
+++ frontend/src/pages/admin/dashboard/ScrapeUrl/Home.tsx
@@ -2,7 +2,7 @@ import ScrapeUrl from "./Components";
 
 export default function ScrapeUrlPage() {
   return (
-    <main className="container h-full max-w-7xl mx-auto py-6 space-y-8 flex flex-col items-center justify-center">
+    <main className="container h-full max-w-7xl mx-auto py-6 space-y-8 flex flex-col">
       <ScrapeUrl/>
     </main>
   )
